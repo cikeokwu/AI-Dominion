e3de5509fa3f339b33f2bcc86c72fcc7f47a6bf0
Kendra Lockard <klockard@oberlin.edu>
2019-05-01 13:45:28 -0400

    assault

---
+++ assault/assault.py
@@ -0,0 +1,39 @@
+import gym
+from dqnagent import DQNAgent
+import numpy as np
+
+EPISODES = 1000
+RENDER = True
+
+
+if __name__ == "__main__":
+    env = gym.make('VideoPinball-v0')
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    agent = DQNAgent(state_size, action_size)
+    #agent.load("assault-dqn.h5")
+    done = False
+    batch_size = 32
+
+    for e in range(EPISODES):
+        state = env.reset()
+        state = np.reshape(state, [-1, state_size])
+
+        for time_t in range(500):
+            #if RENDER:
+                #env.render()
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            reward = reward if not done else -10 #penalize for taking actions when done
+            next_state = np.reshape(next_state, [-31, state_size])
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+
+            if done:
+                print("episode: {}/{}, score: {}".format(e, EPISODES, time_t))
+                break
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
+
+        if e % 10 == 0:
+            agent.save("./assault-dqn.h5")
